4ab3cf4435316f83e417d28801c7c1a8a091f61c
Alicutun <xxtrantienphatxx@gmail.com>
2022-11-01 11:49:32 +0700

    update day 11

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -5,7 +5,7 @@ import Movie from "../pages/Movie.jsx";
 import Event from "../pages/Event.jsx";
 import MovieDetail from "../pages/MovieDetail.jsx";
 import Layout from "../components/admin/Layout.jsx";
-import UserList from "../components/admin/page/userList/UserList";
+import UserList from "../components/admin/page/userList/UserList"; ///lam
 import User from "../components/admin/page/user/User";
 import NewUser from "../components/admin/page/newUser/NewUser";
 import ProductsList from "../components/admin/page/productList/ProductsList";
@@ -21,20 +21,13 @@ const Routes = () => {
       <Route path="/Event" element={<Event />} />
       <Route path="/MovieDetail/:id" element={<MovieDetail />} />
       <Route path="/HomeAdmin" element={<Layout />} />
-      <Route path="/HomeAdmin/users" element={<UserList/>}>
-      </Route>  
-      <Route path="/HomeAdmin/user/:userId" element={<User/>}>
-      </Route>          
-      <Route path="/HomeAdmin/newUser" element={<NewUser/>}>
-      </Route>  
-      <Route path="/HomeAdmin/movies" element={<ProductsList/>}>
-      </Route>  
-      <Route path="/HomeAdmin/movies/:movieId" element={<Product/>}>
-      </Route>          
-      <Route path="/HomeAdmin/newMovie" element={<NewProduct/>}>
-      </Route> 
-      <Route path="/HomeAdmin/newShowing" element={<NewShowing/>}>
-      </Route> 
+      <Route path="/HomeAdmin/users" element={<UserList />}></Route>
+      <Route path="/HomeAdmin/user/:userId" element={<User />}></Route>
+      <Route path="/HomeAdmin/newUser" element={<NewUser />}></Route>
+      <Route path="/HomeAdmin/movies" element={<ProductsList />}></Route>
+      <Route path="/HomeAdmin/movies/:movieId" element={<Product />}></Route>
+      <Route path="/HomeAdmin/newMovie" element={<NewProduct />}></Route>
+      <Route path="/HomeAdmin/newShowing" element={<NewShowing />}></Route>
     </Rou>
   );
 };
